8e44a6b999270641302763b3a6077a8548ced691
cuongtn98 <cuonghoa1998@gmail.com>
2022-12-02 17:13:21 +0700

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       int i = 10;
+       float f = 20.5f;
+       double d = 20.5;
+       boolean b = true ;
+       char c = 'a';
+       String s = "hà nội";
+        System.out.println("i =" + " "+i);
+       System.out.println("float ="+ " "+f);
+        System.out.println("double ="+" " +d);
+        System.out.println("boolean ="+" " +b);
+        System.out.println("char ="+" " +c);
+       System.out.println("String ="+" " + s);
+
     }
 }
