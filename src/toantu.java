e1116193a9e472cb10d644afe10e77eebb1c7040
cuongtn98 <cuonghoa1998@gmail.com>
2022-12-05 08:53:52 +0700

    commit

--- src/toantu.java
+++ src/toantu.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class toantu {
+import java.util.Scanner;
+
+public class toantu {
+    public static void main(String[] args) {
+        float with;
+        float height;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhap with");
+        with = scanner.nextFloat();
+        System.out.println("nhap height");
+        height = scanner.nextFloat();
+        float area = with * height ;
+        System.out.println(area);
+    }
 }
+
